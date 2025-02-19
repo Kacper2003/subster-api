10740416c99201a0fd6a4d3677cf8ca5731c9ed6
Kacper Kaczynski <54663437+Kacper2003@users.noreply.github.com>
2025-02-19 14:13:03 +0000

    Database context setup with initial User entity

--- Subster.API/Program.cs
+++ Subster.API/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Subster.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<SubsterDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("SubsterDb")
+    )
+);
 
 var app = builder.Build();
 
---
+++ Subster.DAL/SubsterDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Subster.DAL.Entities;
+
+namespace Subster.DAL;
+
+public class SubsterDbContext : DbContext
+{
+    public SubsterDbContext(DbContextOptions<SubsterDbContext> options)
+         : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
